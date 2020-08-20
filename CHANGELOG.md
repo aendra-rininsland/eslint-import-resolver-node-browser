18dad628d95a0e894c62c38082499daa2d4fb71b
Ændrew Rininsland <andrew.rininsland@ft.com>
2020-08-20 20:08:17 +0100

    Initial

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Change Log
+All notable changes to this resolver will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/).
+This change log adheres to standards from [Keep a CHANGELOG](http://keepachangelog.com).
+
+## Unreleased
+
+## v0.0.1 - 2020-08-20
---
+++ index.js
@@ -0,0 +1,51 @@
+var resolve = require('resolve')
+  , path = require('path')
+
+var log = require('debug')('eslint-plugin-import:resolver:node')
+
+exports.interfaceVersion = 2
+
+exports.resolve = function (source, file, config) {
+  log('Resolving:', source, 'from:', file)
+  var resolvedPath
+
+  if (resolve.isCore(source)) {
+    log('resolved to core')
+    return { found: true, path: null }
+  }
+
+  try {
+    resolvedPath = resolve.sync(source, opts(file, config))
+    log('Resolved to:', resolvedPath)
+    return { found: true, path: resolvedPath }
+  } catch (err) {
+    log('resolve threw error:', err)
+    return { found: false }
+  }
+}
+
+function opts(file, config) {
+  return Object.assign({
+      // more closely matches Node (#333)
+      // plus 'mjs' for native modules! (#939)
+      extensions: ['.mjs', '.js', '.json', '.node'],
+    },
+    config,
+    {
+      // path.resolve will handle paths relative to CWD
+      basedir: path.dirname(path.resolve(file)),
+      packageFilter: pkg => packageFilter(pkg, cfg),
+
+    })
+}
+
+function packageFilter(pkg, cfg) {
+  if (pkg['jsnext:main']) {
+    pkg['main'] = pkg['jsnext:main']
+  } else if (pkg['browser']) { // Hmm we should make t
+    pkg['main'] = pkg['browser']
+  } else if (cfg && cfg.mainField && pkg[cfg.mainField]) {
+    pkg['main'] = pkg[cfg.mainField]
+  }
+  return pkg
+}
